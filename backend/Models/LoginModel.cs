dd6e3a72fc715fda989b098551bf193ececee1ea
mohithsai07 <mohithsaireddy8@gmail.com>
2026-05-28 17:58:51 +0530

    Make Email and PasswordHash properties nullable

--- backend/Models/LoginModel.cs
+++ backend/Models/LoginModel.cs
@@ -2,8 +2,8 @@ namespace LeaveManagementAPI.Models
 {
     public class LoginModel
     {
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string PasswordHash { get; set; }
+        public string? PasswordHash { get; set; }
     }
 }
